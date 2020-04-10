84a8366a0cd1894c94ea055bc453f83931b7ec23
Daniel Nascimento <dbnkocao@gmail.com>
2020-04-10 10:00:07 -0300

    adding validations and customizing index

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,7 +1,13 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+  validates :client, presence: true
+  
   def fae_display_field
     
+  end
+
+  def self.for_fae_index
+    order(:date)
   end
 
   belongs_to :discount
